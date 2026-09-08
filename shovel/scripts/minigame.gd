b88c910839febc30790eb78dbf9ca2caea62db8f
zvv <zvv@microteh.net>
2026-09-08 16:24:43 +0300

    minigame some

--- shovel/scripts/minigame.gd
+++ shovel/scripts/minigame.gd
@@ -5,8 +5,7 @@ signal finished
 func _ready() -> void:
 	print("i am readyy!")
 	
-	await get_tree().create_timer(3.0).timeout
-	finished.emit()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
